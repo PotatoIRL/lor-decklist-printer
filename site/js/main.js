fb3f72f4e7f7fcbc717c3266415a6641514243e7
PotatoIRL <howishoopwoop@gmail.com>
2020-05-12 18:54:01 -0400

    add deck viewer

--- site/js/main.js
+++ site/js/main.js
@@ -1,27 +1,57 @@
+// Load DeckEncoder from runeterra module
 const {
   DeckEncoder
 } = require('runeterra')
 
-const set = [
+// Load card data mapped as (CARD_ID -> CARD)
+const cards = [
   require('data/set1.json'),
   require('data/set2.json')
-]
-
-const cards = set.reduce(function(map, data) {
+].reduce(function(map, data) {
   data.forEach(item => map[item.cardCode] = item);
   return map;
 }, {})
 
+// Application constants and variables
 const deckcode = document.getElementById("deckcode");
 const decklist = document.getElementById("decklist")
+const deckview = document.getElementById("deckview")
 
+var deck;
+  
+// Global functions
 global.updateDecklist = function() {
-  const deck = DeckEncoder.decode(deckcode.value);
-  deck.sort((x, y) => cards[x.code].cost - cards[y.code].cost)
-  decklist.value = deck.map(x => `${x.count} ${cards[x.code].name} (${cards[x.code].cost})`).join("\n")
+  try {
+    deck = DeckEncoder.decode(deckcode.value);
+    deck.forEach(x => x.card = cards[x.code])
+    deck.sort((x, y) => cards[x.code].cost - cards[y.code].cost)
+    decklist.value = deck
+      .map(x => `${x.count} ${x.card.name} (${x.card.cost})`)
+      .join("\n");
+    deckview.innerHTML = deck
+      .map(x => `\
+        <a href="https://lor.mobalytics.gg/cards/${x.code}" target="_blank">\
+          <svg class="card">\
+            <image width="100%" height="100%" href="${x.card.assets[0].gameAbsolutePath}">x${x.count}</image>\
+            <text x="70%" y="15%" textLength="20%">x${x.count}</text>\
+          </svg>\
+        </a>`)
+      .join("\n");
+  } catch (ex) {
+  }
 }
 
 global.copyDecklist = function() {
-    decklist.select();
-    document.execCommand('copy');
+  decklist.select();
+  document.execCommand('copy');
 }
+
+// Context access
+global.context = function() {
+  return {
+    cards: cards
+  };
+}
+
+// Focus deckcode
+deckcode.focus();
--- site/js/main.standalone.js
+++ site/js/main.standalone.js
@@ -22945,33 +22945,63 @@ module.exports = {
 
 },{"./Card":4,"./DeckEncoder":5,"./Faction":6}],9:[function(require,module,exports){
 (function (global){
+// Load DeckEncoder from runeterra module
 const {
   DeckEncoder
 } = require('runeterra')
 
-const set = [
+// Load card data mapped as (CARD_ID -> CARD)
+const cards = [
   require('data/set1.json'),
   require('data/set2.json')
-]
-
-const cards = set.reduce(function(map, data) {
+].reduce(function(map, data) {
   data.forEach(item => map[item.cardCode] = item);
   return map;
 }, {})
 
+// Application constants and variables
 const deckcode = document.getElementById("deckcode");
 const decklist = document.getElementById("decklist")
+const deckview = document.getElementById("deckview")
 
+var deck;
+  
+// Global functions
 global.updateDecklist = function() {
-  const deck = DeckEncoder.decode(deckcode.value);
-  deck.sort((x, y) => cards[x.code].cost - cards[y.code].cost)
-  decklist.value = deck.map(x => `${x.count} ${cards[x.code].name} (${cards[x.code].cost})`).join("\n")
+  try {
+    deck = DeckEncoder.decode(deckcode.value);
+    deck.forEach(x => x.card = cards[x.code])
+    deck.sort((x, y) => cards[x.code].cost - cards[y.code].cost)
+    decklist.value = deck
+      .map(x => `${x.count} ${x.card.name} (${x.card.cost})`)
+      .join("\n");
+    deckview.innerHTML = deck
+      .map(x => `\
+        <a href="https://lor.mobalytics.gg/cards/${x.code}" target="_blank">\
+          <svg class="card">\
+            <image width="100%" height="100%" href="${x.card.assets[0].gameAbsolutePath}">x${x.count}</image>\
+            <text x="70%" y="15%" textLength="20%">x${x.count}</text>\
+          </svg>\
+        </a>`)
+      .join("\n");
+  } catch (ex) {
+  }
 }
 
 global.copyDecklist = function() {
-    decklist.select();
-    document.execCommand('copy');
+  decklist.select();
+  document.execCommand('copy');
 }
+
+// Context access
+global.context = function() {
+  return {
+    cards: cards
+  };
+}
+
+// Focus deckcode
+deckcode.focus();
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{"data/set1.json":1,"data/set2.json":2,"runeterra":8}]},{},[9]);
