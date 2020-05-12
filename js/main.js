c41bf703d0ee05a543e055551fbd932900297b73
PotatoIRL <howishoopwoop@gmail.com>
2020-05-12 14:22:17 -0400

    js changes

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
-const { DeckEncoder } = require('runeterra')
+const {
+  DeckEncoder
+} = require('runeterra')
+
+set = [
+  require('data/set1.json'),
+  require('data/set2.json')
+]
+
+cards = set.reduce(function(map, data) {
+  data.forEach(item => map[item.cardCode] = item);
+  return map;
+}, {})
 
 global.updateDecklist = function() {
   const deckcode = document.getElementById("deckcode").value;
   const deck = DeckEncoder.decode(deckcode);
   const decklist = document.getElementById("decklist")
-  decklist.value = deck.map(x => x.count + " " + x.code).join("\n")
+  decklist.value = deck.map(x => x.count + " " + cards[x.code].name + "(" + cards[x.code].cost + ")").join("\n")
 }
